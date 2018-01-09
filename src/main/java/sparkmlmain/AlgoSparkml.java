e3b91bea60c1f371ec9ed8d3437fae267f64dbb6
juliette <juliette@ZBook-18>
2018-01-09 16:23:19 +0100

    test spark

--- src/main/java/sparkmlmain/AlgoSparkml.java
+++ src/main/java/sparkmlmain/AlgoSparkml.java
@@ -1,4 +1,5 @@
 package sparkmlmain;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,49 +15,42 @@ import org.apache.spark.mllib.tree.model.DecisionTreeModel;
 import org.apache.spark.mllib.util.MLUtils;
 
 
-
 public class AlgoSparkml {
 
 	public static void main(String[] args) {
-		SparkConf sparkConf = new SparkConf().setAppName("JavaDecisionTreeRegressionExample");
-		JavaSparkContext jsc = new JavaSparkContext(sparkConf);
+		
+	    SparkConf sparkConf = new SparkConf().setAppName("AlgoSparkml").setMaster("local");
+	    JavaSparkContext jsc = new JavaSparkContext(sparkConf);
+	    // Load and parse the data file.
+	    String datapath = "data/mllib/sample_libsvm_data.txt";
+	    JavaRDD<LabeledPoint> data = MLUtils.loadLibSVMFile(jsc.sc(), datapath).toJavaRDD();
+	    // Split the data into training and test sets (30% held out for testing)
+	    JavaRDD<LabeledPoint>[] splits = data.randomSplit(new double[]{0.7, 0.3});
+	    JavaRDD<LabeledPoint> trainingData = splits[0];
+	    JavaRDD<LabeledPoint> testData = splits[1];
 
-		// Load and parse the data file.
-		String datapath = "resources/iris.csv";
-		JavaRDD<LabeledPoint> data = MLUtils.loadLibSVMFile(jsc.sc(), datapath).toJavaRDD();
-		// Split the data into training and test sets (30% held out for testing)
-		JavaRDD<LabeledPoint>[] splits = data.randomSplit(new double[]{0.7, 0.3});
-		JavaRDD<LabeledPoint> trainingData = splits[0];
-		JavaRDD<LabeledPoint> testData = splits[1];
+	    // Set parameters.
+	    //  Empty categoricalFeaturesInfo indicates all features are continuous.
+	    int numClasses = 2;
+	    Map<Integer, Integer> categoricalFeaturesInfo = new HashMap<>();
+	    String impurity = "gini";
+	    int maxDepth = 5;
+	    int maxBins = 32;
 
-		// Set parameters.
-		// Empty categoricalFeaturesInfo indicates all features are continuous.
-		Map<Integer, Integer> categoricalFeaturesInfo = new HashMap<>();
-		String impurity = "variance";
-		int maxDepth = 5;
-		int maxBins = 32;
+	    // Train a DecisionTree model for classification.
+	    DecisionTreeModel model = DecisionTree.trainClassifier(trainingData, numClasses,
+	      categoricalFeaturesInfo, impurity, maxDepth, maxBins);
 
-		// Train a DecisionTree model.
-		DecisionTreeModel model = DecisionTree.trainRegressor(trainingData,
-				categoricalFeaturesInfo, impurity, maxDepth, maxBins);
+	    // Evaluate model on test instances and compute test error
+	    JavaPairRDD<Double, Double> predictionAndLabel =
+	      testData.mapToPair(p -> new Tuple2<>(model.predict(p.features()), p.label()));
+	    double testErr =
+	      predictionAndLabel.filter(pl -> !pl._1().equals(pl._2())).count() / (double) testData.count();
 
-		// Evaluate model on test instances and compute test error
-		JavaPairRDD<Double, Double> predictionAndLabel =
-				testData.mapToPair(p -> new Tuple2<>(model.predict(p.features()), p.label()));
-		double testMSE = predictionAndLabel.mapToDouble(pl -> {
-			double diff = pl._1() - pl._2();
-			return diff * diff;
-		}).mean();
-		System.out.println("Test Mean Squared Error: " + testMSE);
-		System.out.println("Learned regression tree model:\n" + model.toDebugString());
+	    System.out.println("Test Error: " + testErr);
+	    System.out.println("Learned classification tree model:\n" + model.toDebugString());
 
-		// Save and load model
-		model.save(jsc.sc(), "target/tmp/myDecisionTreeRegressionModel");
-		DecisionTreeModel sameModel = DecisionTreeModel
-				.load(jsc.sc(), "target/tmp/myDecisionTreeRegressionModel");
-		// $example off$
-
-
+	    jsc.close();
 	}
 
 }
