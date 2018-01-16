a42e6ae6707626ad2b26c38c3a36ec3eaacabe38
armel <armel@HP-ZBook-26>
2018-01-16 16:58:27 +0100

    Renjin adapté

--- src/main/java/renjinMain/AlgoRenjin.java
+++ src/main/java/renjinMain/AlgoRenjin.java
@@ -1,6 +1,8 @@
 package renjinMain;
+
 import javax.script.*;
 import org.renjin.script.*;
+import org.renjin.sexp.Vector;
 
 
 public class AlgoRenjin {
@@ -10,7 +12,16 @@ public class AlgoRenjin {
 	// create a Renjin engine:
 	ScriptEngine engine = factory.getScriptEngine();
 	
-	engine.eval(new java.io.FileReader("/home/armel/Documents/Génie logiciel/scriptR.R"));
+	engine.eval(new java.io.FileReader("src/main/resources/scriptR.R"));
+	
+	engine.put("repertoire", "src/main/resources/iris.csv");
+	engine.put("y", "Species");
+	engine.put("splitRatio", 0.7);
+	engine.put("minBucket", 20);
+	Vector accuracyVector = (Vector)engine.eval("arbre_classification(repertoire,y, splitRatio, minBucket)");
+	double accuracy = accuracyVector.getElementAsDouble(0);
+	System.out.println(accuracy);
+
 	
 	}
 
