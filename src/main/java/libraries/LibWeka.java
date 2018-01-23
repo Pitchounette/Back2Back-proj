7133f0ca551a18d0bb0dac088ba908343ecfe199
lydia <lydia@ZBook-12>
2018-01-23 15:19:17 +0100

    retrait comparateur

--- src/main/java/libraries/LibWeka.java
+++ src/main/java/libraries/LibWeka.java
@@ -16,56 +16,61 @@ import weka.filters.unsupervised.attribute.Remove;
 
 public class LibWeka {
 	
-	public String path;
-	public Instances data;
-	public Instances train;
-	public Instances test;
+	private String path;
+	private String name;
+	private Instances train;
+	private Instances test;
 	
-	public LibWeka(String path, boolean header) throws Exception {
+	public LibWeka(String path,String name, boolean header) throws Exception {
 		this.path = path;
+		this.name = name;
 		
-		File file = new File(path);
-		CSVLoader loader = new CSVLoader();
-		loader.setSource(file);
-		this.data = loader.getDataSet();
+		File fileTrain = new File(path+"train_"+name+".csv");
+		CSVLoader loaderTrain = new CSVLoader();
+		loaderTrain.setSource(fileTrain);
+		this.train = loaderTrain.getDataSet();
+		this.train.setClassIndex(this.train.numAttributes() - 1);
 		
-		Instances data = loader.getDataSet();
-		data.setClassIndex(data.numAttributes() - 1);
+		File fileTest = new File(path+"test_"+name+".csv");
+		CSVLoader loaderTest = new CSVLoader();
+		loaderTest.setSource(fileTest);
+		this.test = loaderTest.getDataSet();
+		this.test.setClassIndex(this.test.numAttributes() - 1);
 		
 		if(header) {
-			String[] options = new String[2];
+			 String[] options = new String[2];
 			 options[0] = "-R";                                   
-			 options[1] = "1";                                 
-			 Remove remove = new Remove();                         
-			 remove.setOptions(options);                           
-			 remove.setInputFormat(data);                       
-			 data = Filter.useFilter(data, remove);  
+			 options[1] = "1"; 
+			 
+			 Remove removeTrain = new Remove();                         
+			 removeTrain.setOptions(options);                           
+			 removeTrain.setInputFormat(this.train);                       
+			 this.train = Filter.useFilter(this.train, removeTrain);  
+			 
+			 Remove removeTest = new Remove();                         
+			 removeTest.setOptions(options);                           
+			 removeTest.setInputFormat(this.test);                       
+			 this.test = Filter.useFilter(this.test, removeTest); 
 		}
 	}
 	
-	public void splitData(double pourc) throws IOException {
-		data.randomize(new java.util.Random());
-		
-		int trainTaille = (int) Math.round(data.numInstances() * pourc);
-		int testTaille = data.numInstances() - trainTaille;
-		this.train = new Instances(data, 0, trainTaille);
-		this.test = new Instances(data, trainTaille, testTaille);
-	}
-	
-	
-	
-	
-	public double J48accuracy(Instances trainData, Instances testData) throws Exception {
+	public J48 DecisionTree() throws Exception {
 		J48 tree = new J48();
 		
 		String[] options = new String[1];
 		options[0] = "-U"; 
 		tree.setOptions(options);
 		
-		tree.buildClassifier(trainData);
+		tree.buildClassifier(this.train);
+		
+		return tree;
+	}
+	
+	public double Accuracy(J48 tree) throws Exception {
+		
 
-		Evaluation eval = new Evaluation(trainData);
-		eval.evaluateModel(tree, testData);
+		Evaluation eval = new Evaluation(this.train);
+		eval.evaluateModel(tree, this.test);
 
 		return eval.pctCorrect()/100;
 	}
